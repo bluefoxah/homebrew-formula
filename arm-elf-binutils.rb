de3a96c403643bd8679a80b6c636df4b563ae39d
ZhangYuanhao <bluefoxah@gmail.com>
2016-12-07 23:54:55 +0800

    [MOD] use gcc4.9

--- arm-elf-binutils.rb
+++ arm-elf-binutils.rb
@@ -7,13 +7,13 @@ class ArmElfBinutils < Formula
   version "2.25"
   sha256 "cccf377168b41a52a76f46df18feb8f7285654b3c1bd69fc8265cb0fc6902f2d"
 
-  depends_on 'apple-gcc42' => :build
+  depends_on 'gcc49' => :build
 
   def install
-    ENV['CC'] = '/usr/local/bin/gcc-4.2'
-    ENV['CXX'] = '/usr/local/bin/g++-4.2'
-    ENV['CPP'] = '/usr/local/bin/cpp-4.2'
-    ENV['LD'] = '/usr/local/bin/gcc-4.2'
+    ENV['CC'] = '/usr/local/bin/gcc-4.9'
+    ENV['CXX'] = '/usr/local/bin/g++-4.9'
+    ENV['CPP'] = '/usr/local/bin/cpp-4.9'
+    ENV['LD'] = '/usr/local/bin/gcc-4.9'
 
     mkdir 'build' do
       system '../configure', '--disable-nls', '--target=arm-elf-eabi',
